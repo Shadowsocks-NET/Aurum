0a04cea71da4656d48cd6b7968311c1c57d25ca7
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-09-04 11:16:25 +0800

    feat(configurator): transport/stream config types
    Implemented v2ray's all network settings (except DomainSocket which is obsolete) and StreamSettings.

---
+++ Aurum/Configurator/Configurator.fs
@@ -0,0 +1,13 @@
+﻿namespace Aurum.Configurator
+
+type Protocol =
+    | VLESS
+    | VMess
+    | Shadowsocks
+    | Trojan
+    | NaiveProxy
+
+type Credentials(uuid, encryption) =
+    member this.uuid = uuid
+    member this.encryption = encryption
+    new() = Credentials("", "none")
