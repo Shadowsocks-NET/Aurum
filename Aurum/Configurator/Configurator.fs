b506e9f8317bb8333e0f82c412171b5db2a0075e
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-09-04 21:12:00 +0800

    feat(outbound): implements outbound types Protocols implemented: VMess, VLESS (subject to removal, reasons ref Shadowsocks-NET/v2ray-go), Trojan, Shadowsocks

--- Aurum/Configurator/Configurator.fs
+++ Aurum/Configurator/Configurator.fs
@@ -1,13 +1,1 @@
 ﻿namespace Aurum.Configurator
-
-type Protocol =
-    | VLESS
-    | VMess
-    | Shadowsocks
-    | Trojan
-    | NaiveProxy
-
-type Credentials(uuid, encryption) =
-    member this.uuid = uuid
-    member this.encryption = encryption
-    new() = Credentials("", "none")
