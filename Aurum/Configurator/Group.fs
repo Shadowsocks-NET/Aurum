7c791f64ca68596d8af164710e2fbb35d71eecd1
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-09-11 23:13:34 +0800

    feature(ORM): group and basic connections
    default db is SQLite, using SQLite-PCL library

---
+++ Aurum/Configurator/Group.fs
@@ -0,0 +1,59 @@
+﻿namespace Aurum
+
+open FSharp.Json
+open SQLite
+open SQLitePCL
+
+module Group =
+    type ConnectionRecordObject =
+        { id: string
+          name: string
+          tags: string list }
+
+    type SubscriptionType =
+        | None
+        | Base64
+        | SIP008
+        | OOCv1
+
+    type SubscriptionObject =
+        { [<JsonField("type")>]
+          subscriptionType: SubscriptionType
+          source: string option }
+
+    type GroupObject =
+        { id: string
+          name: string
+          subscription: SubscriptionType
+          subscriptionSource: string option
+          connections: string list (* stores id of connections belong to this group *)  }
+
+    [<Table("Tags")>]
+    type Tags(tag: string, nodeId: string) =
+        [<PrimaryKey>]
+        [<Column("tag")>]
+        member this.tag = tag
+
+        [<Column("nodeId")>]
+        member this.nodeId = nodeId
+
+    [<Table("Connections")>]
+    type Connections(name: string, id: string, configuration: string, connType: string, host: string, port: string) =
+        [<PrimaryKey>]
+        [<Column("id")>]
+        member this.id = id
+
+        [<Column("name")>]
+        member this.name = name
+
+        [<Column("configuration")>]
+        member this.configuration = configuration
+
+        [<Column("type")>]
+        member this.connType = connType
+
+        [<Column("host")>]
+        member this.host = host
+
+        [<Column("port")>]
+        member this.port = port
