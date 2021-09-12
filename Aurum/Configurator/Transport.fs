ec64326534623e385278cea6223f9563edb6aad1
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-09-12 22:37:39 +0800

    feature(transport): WebSocketObject constructor

--- Aurum/Configurator/Transport.fs
+++ Aurum/Configurator/Transport.fs
@@ -1,126 +1,158 @@
-namespace Aurum.Configurator.Transport
+namespace Aurum.Configurator
 
 open System.Collections.Generic
 open FSharp.Json
 
-type Networks =
-    | WS
-    | GRPC
-    | TCP
-    | KCP
-    | DomainSocket
-    | HTTP
-    | QUIC
+module Transport =
+    [<RequireQualifiedAccess>]
+    type Networks =
+        | WS
+        | GRPC
+        | TCP
+        | KCP
+        | DomainSocket
+        | HTTP
+        | QUIC
 
+    [<RequireQualifiedAccess>]
+    type Security =
+        | None
+        | TLS
+        | XTLS
 
-type Security =
-    | None
-    | TLS
-    | XTLS
+    type WebSocketObject =
+        { path: string
+          maxEarlyData: int
+          browserForwarding: bool
+          earlyDataHeader: string
+          headers: Dictionary<string, string> option }
 
-type WebSocketObject =
-    { path: string
-      maxEarlyData: int
-      browserForwarding: bool
-      earlyDataHeader: string
-      headers: Dictionary<string, string> }
+    let createWebSocketObject
+        (
+            path: string option,
+            maxEarlyData: int option,
+            browserForwarding: bool option,
+            earlyDataHeader: string option,
+            headers: Dictionary<string, string> option
+        ) =
+        { WebSocketObject.path =
+              match path with
+              | None -> "/"
+              | Some path -> path
+          maxEarlyData =
+              match maxEarlyData with
+              | None -> 0
+              | Some maxEarlyData -> maxEarlyData
+          browserForwarding =
+              match browserForwarding with
+              | None -> false
+              | Some browserForwarding -> browserForwarding
+          earlyDataHeader =
+              match earlyDataHeader with
+              | None -> ""
+              | Some earlyDataHeader -> earlyDataHeader
+          headers = headers }
 
-type HttpRequestObject =
-    { version: string
-      method: string
-      path: string list
-      headers: Dictionary<string, string list> }
+    type HttpRequestObject =
+        { version: string
+          method: string
+          path: string list
+          headers: Dictionary<string, string list> }
 
-type HttpResponseObject =
-    { version: string
-      status: string
-      reason: string
-      headers: Dictionary<string, string list> }
+    type HttpResponseObject =
+        { version: string
+          status: string
+          reason: string
+          headers: Dictionary<string, string list> }
 
-type TcpHeaderObject =
-    { [<JsonField("type")>]
-      headerType: string
-      request: HttpRequestObject
-      response: HttpResponseObject }
+    type TcpHeaderObject =
+        { [<JsonField("type")>]
+          headerType: string
+          request: HttpRequestObject
+          response: HttpResponseObject }
 
-type TcpObject = { header: TcpHeaderObject }
+    type TcpObject = { header: TcpHeaderObject }
 
-type KcpHeaders =
-    | None
-    | SRTP
-    | UTP
-    | WechatVideo
-    | DTLS
-    | WireGuard
+    [<RequireQualifiedAccess>]
+    type KcpHeaders =
+        | None
+        | SRTP
+        | UTP
+        | WechatVideo
+        | DTLS
+        | WireGuard
 
-type UdpHeaderObject =
-    { [<JsonField("type")>]
-      headerType: KcpHeaders }
+    type UdpHeaderObject =
+        { [<JsonField("type")>]
+          headerType: KcpHeaders }
 
-type KcpObject =
-    { mtu: int
-      tti: int
-      uplinkCapacity: int
-      downlinkCapacity: int
-      congestion: bool
-      readBufferSize: int
-      writeBufferSize: int
-      header: UdpHeaderObject
-      seed: string }
+    type KcpObject =
+        { mtu: int
+          tti: int
+          uplinkCapacity: int
+          downlinkCapacity: int
+          congestion: bool
+          readBufferSize: int
+          writeBufferSize: int
+          header: UdpHeaderObject
+          seed: string }
 
-type HTTPMethod =
-    | GET
-    | HEAD
-    | POST
-    | PUT
-    | DELETE
-    | CONNECT
-    | OPTIONS
-    | TRACE
-    | PATCH
+    [<RequireQualifiedAccess>]
+    type HTTPMethod =
+        | GET
+        | HEAD
+        | POST
+        | PUT
+        | DELETE
+        | CONNECT
+        | OPTIONS
+        | TRACE
+        | PATCH
 
-type HttpObject =
-    { host: string list
-      path: string
-      method: HTTPMethod
-      headers: Dictionary<string, string list> }
+    type HttpObject =
+        { host: string list
+          path: string
+          method: HTTPMethod
+          headers: Dictionary<string, string list> }
 
-// reserved for future annotations.
-type QuicSecurity =
-    | None
-    | [<JsonField("aes-128-gcm")>] AES
-    | [<JsonField("chacha20-poly1305")>] ChaCha20
+    // reserved for future annotations.
+    [<RequireQualifiedAccess>]
+    type QuicSecurity =
+        | None
+        | [<JsonField("aes-128-gcm")>] AES
+        | [<JsonField("chacha20-poly1305")>] ChaCha20
 
-type QuicObject =
-    { security: string
-      key: string
-      header: UdpHeaderObject }
+    type QuicObject =
+        { security: string
+          key: string
+          header: UdpHeaderObject }
 
-type GrpcObject = { serviceName: string }
+    type GrpcObject = { serviceName: string }
 
-type TLSObject =
-    { serverName: string
-      allowInsecure: bool
-      alpn: string list
-      disableSystemRoot: bool }
+    type TLSObject =
+        { serverName: string
+          allowInsecure: bool
+          alpn: string list
+          disableSystemRoot: bool }
 
-type TProxyType =
-    | Redirect
-    | TProxy
-    | Off
+    [<RequireQualifiedAccess>]
+    type TProxyType =
+        | Redirect
+        | TProxy
+        | Off
 
-type SockoptObject =
-    { mark: int
-      tcpFastOpen: bool option
-      tproxy: TProxyType }
+    type SockoptObject =
+        { mark: int
+          tcpFastOpen: bool option
+          tproxy: TProxyType }
 
-type StreamSettingsObject =
-    { network: Networks
-      tls: TLSObject option
-      tcp: TcpObject option
-      kcp: KcpObject option
-      ws: WebSocketObject option
-      http: HttpObject option
-      quic: QuicObject option
-      grpc: GrpcObject option
-      sockopt: SockoptObject option }
+    type StreamSettingsObject =
+        { network: Networks
+          tls: TLSObject option
+          tcp: TcpObject option
+          kcp: KcpObject option
+          ws: WebSocketObject option
+          http: HttpObject option
+          quic: QuicObject option
+          grpc: GrpcObject option
+          sockopt: SockoptObject option }
