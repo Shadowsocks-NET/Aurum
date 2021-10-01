08fe757494e55b46981bf3aaffa7680e92923ac2
shanoaice <25174488+shanoaice@users.noreply.github.com>
2021-10-01 23:26:18 +0800

    refactor(modules): tidy up namespaces and modules

--- Aurum/Configurator/Inbound.fs
+++ Aurum/Configurator/Inbound.fs
@@ -1,48 +1,48 @@
-module Aurum.Configurator.Inbound
+namespace Aurum.Configurator
 
 open FSharp.Json
 
-// reserved for future annotations.
-type Protocols =
-    | [<JsonUnionCase("http")>] HTTP
-    | [<JsonUnionCase("socks")>] Socks
-    | [<JsonUnionCase("dokodemo-door")>] DokodemoDoor
+module Inbound =
+    type Protocols =
+        | [<JsonUnionCase("http")>] HTTP
+        | [<JsonUnionCase("socks")>] Socks
+        | [<JsonUnionCase("dokodemo-door")>] DokodemoDoor
 
-type SocksAuth =
-    | [<JsonUnionCase("noauth")>] NoAuth
-    | [<JsonUnionCase("password")>] Password
+    type SocksAuth =
+        | [<JsonUnionCase("noauth")>] NoAuth
+        | [<JsonUnionCase("password")>] Password
 
-type AccountObject = { User: string; Pass: string }
+    type AccountObject = { User: string; Pass: string }
 
-type InboundConfigurationObject =
-    { Accounts: AccountObject list
-      UserLevel: int
-      Timeout: int
-      (*HTTP specific*)
-      AllowTransparent: bool
-      (*SOCKS specific*)
-      Auth: SocksAuth
-      UDP: bool
-      IP: string
-      (*Dokodemo-door specific*)
-      Address: string option
-      Port: int
-      Network: string
-      FollowRedirect: bool }
+    type InboundConfigurationObject =
+        { Accounts: AccountObject list
+          UserLevel: int
+          Timeout: int
+          (*HTTP specific*)
+          AllowTransparent: bool
+          (*SOCKS specific*)
+          Auth: SocksAuth
+          UDP: bool
+          IP: string
+          (*Dokodemo-door specific*)
+          Address: string option
+          Port: int
+          Network: string
+          FollowRedirect: bool }
 
-type SniffingObject =
-    { Enabled: bool
-      DestOverride: string list
-      MetadataOnly: bool }
+    type SniffingObject =
+        { Enabled: bool
+          DestOverride: string list
+          MetadataOnly: bool }
 
-type InboundObject =
-    { Listen: string
-      Port: int
-      Protocol: string
-      Settings: InboundConfigurationObject
-      Tag: string
-      Sniffing: SniffingObject }
+    type InboundObject =
+        { Listen: string
+          Port: int
+          Protocol: string
+          Settings: InboundConfigurationObject
+          Tag: string
+          Sniffing: SniffingObject }
 
-let createSocksAccount user pass =
-    { AccountObject.User = user
-      Pass = pass }
+    let createSocksAccount user pass =
+        { AccountObject.User = user
+          Pass = pass }
--- Aurum/Configurator/Outbound.fs
+++ Aurum/Configurator/Outbound.fs
@@ -1,97 +1,98 @@
-module Aurum.Configurator.Outbound
+namespace Aurum.Configurator
 
 open FSharp.Json
 open Aurum.Configurator.Transport
 
-[<RequireQualifiedAccessAttribute>]
-type ShadowsocksEncryption =
-    | [<JsonUnionCase("none")>] None
-    | [<JsonUnionCase("plain")>] Plain
-    | [<JsonUnionCase("chacha20-poly1305")>] ChaCha20
-    | [<JsonUnionCase("chacha20-ietf-poly1305")>] ChaCha20IETF
-    | [<JsonUnionCase("aes-128-gcm")>] AES128
-    | [<JsonUnionCase("aes-256-gcm")>] AES256
+module Outbound =
+    [<RequireQualifiedAccessAttribute>]
+    type ShadowsocksEncryption =
+        | [<JsonUnionCase("none")>] None
+        | [<JsonUnionCase("plain")>] Plain
+        | [<JsonUnionCase("chacha20-poly1305")>] ChaCha20
+        | [<JsonUnionCase("chacha20-ietf-poly1305")>] ChaCha20IETF
+        | [<JsonUnionCase("aes-128-gcm")>] AES128
+        | [<JsonUnionCase("aes-256-gcm")>] AES256
 
-[<RequireQualifiedAccessAttribute>]
-// VLESS is removed in v2ray-go, so this is subject to removal too (may happen in any time).
-type VLESSEncryption = | [<JsonUnionCase("none")>] None
+    [<RequireQualifiedAccessAttribute>]
+    // VLESS is removed in v2ray-go, so this is subject to removal too (may happen in any time).
+    type VLESSEncryption = | [<JsonUnionCase("none")>] None
 
-type Protocols =
-    | [<JsonUnionCase("vless")>] VLESS // subject to removal.
-    | [<JsonUnionCase("vmess")>] VMess
-    | [<JsonUnionCase("shadowsocks")>] Shadowsocks
-    | [<JsonUnionCase("trojan")>] Trojan
+    type Protocols =
+        | [<JsonUnionCase("vless")>] VLESS // subject to removal.
+        | [<JsonUnionCase("vmess")>] VMess
+        | [<JsonUnionCase("shadowsocks")>] Shadowsocks
+        | [<JsonUnionCase("trojan")>] Trojan
 
-[<RequireQualifiedAccessAttribute>]
-type VMessEncryption =
-    | [<JsonUnionCase("none")>] None
-    | [<JsonUnionCase("zero")>] Zero
-    | [<JsonUnionCase("auto")>] Auto
-    | [<JsonUnionCase("aes-128-gcm")>] AES
-    | [<JsonUnionCase("chacha20-poly1305")>] ChaCha20
+    [<RequireQualifiedAccessAttribute>]
+    type VMessEncryption =
+        | [<JsonUnionCase("none")>] None
+        | [<JsonUnionCase("zero")>] Zero
+        | [<JsonUnionCase("auto")>] Auto
+        | [<JsonUnionCase("aes-128-gcm")>] AES
+        | [<JsonUnionCase("chacha20-poly1305")>] ChaCha20
 
-type UserObject =
-    { ID: string
-      Encryption: VLESSEncryption option
-      Level: int option
-      AlterId: int
-      Security: VMessEncryption option }
+    type UserObject =
+        { ID: string
+          Encryption: VLESSEncryption option
+          Level: int option
+          AlterId: int
+          Security: VMessEncryption option }
 
-// v2ray-go specific implementation, removed VLESS components.
-type GoUserObject =
-    { ID: string
-      Level: int option
-      Security: VMessEncryption }
+    // v2ray-go specific implementation, removed VLESS components.
+    type GoUserObject =
+        { ID: string
+          Level: int option
+          Security: VMessEncryption }
 
-type ServerObject =
-    { Address: string
-      Port: int
-      Password: string option
-      Email: string option
-      Level: int option
-      Method: ShadowsocksEncryption option
-      IvCheck: bool option
-      Users: UserObject list }
+    type ServerObject =
+        { Address: string
+          Port: int
+          Password: string option
+          Email: string option
+          Level: int option
+          Method: ShadowsocksEncryption option
+          IvCheck: bool option
+          Users: UserObject list }
 
-type OutboundConfigurationObject =
-    { Vnext: ServerObject list option
-      Servers: ServerObject list option }
+    type OutboundConfigurationObject =
+        { Vnext: ServerObject list option
+          Servers: ServerObject list option }
 
-// v2ray-go specific implementation, removed vnext layer.
-type GoOutboundConfigurationObject =
-    { Address: string
-      Port: int
-      Users: GoUserObject list
-      Servers: ServerObject list }
+    // v2ray-go specific implementation, removed vnext layer.
+    type GoOutboundConfigurationObject =
+        { Address: string
+          Port: int
+          Users: GoUserObject list
+          Servers: ServerObject list }
 
-type MuxObject = { Enabled: bool; Concurrency: int }
+    type MuxObject = { Enabled: bool; Concurrency: int }
 
-type GenericOutboundObject<'T> =
-    { SendThrough: string
-      Protocol: Protocols
-      Settings: 'T
-      Tag: string
-      StreamSettings: StreamSettingsObject
-      Mux: MuxObject }
+    type GenericOutboundObject<'T> =
+        { SendThrough: string
+          Protocol: Protocols
+          Settings: 'T
+          Tag: string
+          StreamSettings: StreamSettingsObject
+          Mux: MuxObject }
 
-type OutboundObject = GenericOutboundObject<OutboundConfigurationObject>
-// v2ray-go specific implementation, removed vnext and VLESS.
-type GoOutboundObject = GenericOutboundObject<GoOutboundConfigurationObject>
+    type OutboundObject = GenericOutboundObject<OutboundConfigurationObject>
+    // v2ray-go specific implementation, removed vnext and VLESS.
+    type GoOutboundObject = GenericOutboundObject<GoOutboundConfigurationObject>
 
 
-let createVMessUserObject uuid security level alterId =
-    { UserObject.ID = uuid
-      Security = security
-      Level = level
-      AlterId = Option.defaultValue 0 alterId
-      Encryption = None }
+    let createVMessUserObject uuid security level alterId =
+        { UserObject.ID = uuid
+          Security = security
+          Level = level
+          AlterId = Option.defaultValue 0 alterId
+          Encryption = None }
 
-let createVMessServerObject host port users =
-    { ServerObject.Address = host
-      Port = port
-      Users = users
-      Password = None
-      Email = None
-      Level = None
-      Method = None
-      IvCheck = None }
+    let createVMessServerObject host port users =
+        { ServerObject.Address = host
+          Port = port
+          Users = users
+          Password = None
+          Email = None
+          Level = None
+          Method = None
+          IvCheck = None }
