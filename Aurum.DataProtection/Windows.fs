73e848c2187153ceacf02a3e76ce75a74ca28aca
shanoaice <25174488+shanoaice@users.noreply.github.com>
2021-10-04 22:45:01 +0800

    feature(protection): Windows-only DataProtection Wrapper

--- Aurum.DataProtection/Windows.fs
+++ Aurum.DataProtection/Windows.fs
@@ -1,4 +1,15 @@
 namespace Aurum.DataProtection
 
 module Windows =
-    let placeholder = ()
+    let encryptString (data: string) =
+        let rawBytes = System.Text.Encoding.UTF8.GetBytes data
+
+        System.Security.Cryptography.ProtectedData.Protect
+            rawBytes
+            System.Security.Cryptography.DataProtectionScope.CurrentUser
+
+    let decryptString protected =
+        let decryptedBytes = System.Security.Cryptography.ProtectedData.Unprotect
+            protected
+            System.Security.Cryptography.DataProtectionScope.CurrentUser
+        System.Text.Encoding.UTF8.GetString decryptedBytes
