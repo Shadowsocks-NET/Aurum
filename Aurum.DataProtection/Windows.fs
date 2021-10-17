61ac3e59e3de7389f691f1d1b187418961bf5b83
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-10-17 10:29:38 +0800

    fix(dataProtection): use correct tuple form

--- Aurum.DataProtection/Windows.fs
+++ Aurum.DataProtection/Windows.fs
@@ -4,12 +4,18 @@ module Windows =
     let encryptString (data: string) =
         let rawBytes = System.Text.Encoding.UTF8.GetBytes data
 
-        System.Security.Cryptography.ProtectedData.Protect
-            rawBytes
+        System.Security.Cryptography.ProtectedData.Protect(
+            rawBytes,
+            null,
             System.Security.Cryptography.DataProtectionScope.CurrentUser
+        )
 
-    let decryptString protected =
-        let decryptedBytes = System.Security.Cryptography.ProtectedData.Unprotect
-            protected
-            System.Security.Cryptography.DataProtectionScope.CurrentUser
+    let decryptString protectedData =
+        let decryptedBytes =
+            System.Security.Cryptography.ProtectedData.Unprotect(
+                protectedData,
+                null,
+                System.Security.Cryptography.DataProtectionScope.CurrentUser
+            )
+
         System.Text.Encoding.UTF8.GetString decryptedBytes
