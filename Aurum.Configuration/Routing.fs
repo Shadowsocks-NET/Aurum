4d47b396a73234c65a467c846363229188008696
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-11-11 21:07:00 +0800

    wip(routing): RuleObject

--- Aurum.Configuration/Routing.fs
+++ Aurum.Configuration/Routing.fs
@@ -2,3 +2,23 @@ module Aurum.Configuration.Routing
 
 open Aurum
 open Aurum.Configuration
+open FSharp.Json
+
+type DomainStrategy =
+    | [<JsonUnionCase("AsIs")>] AsIs
+    | [<JsonUnionCase("IPIfNonMatch")>] IPIfNonMatch
+    | [<JsonUnionCase("IPOnDemand")>] IPOnDemand
+
+type DomainMatcher =
+    | [<JsonUnionCase("mph")>] MinimalPerfectHash
+    | [<JsonUnionCase("linear")>] Original
+
+type RuleObjectType = | [<JsonUnionCase("field")>] Field
+
+type RuleObject =
+    { DomainMatcher: DomainMatcher option
+      Type: RuleObjectType
+      Domains: string list option
+      IP: string list option
+      Port: string option
+      SourcePort: string option }
