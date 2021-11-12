543211e99d057332c654047c54a9972a5d989b60
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-11-12 15:51:45 +0800

    feature(routing): generate rules with full list

--- Aurum.Configuration/Routing.fs
+++ Aurum.Configuration/Routing.fs
@@ -20,6 +20,11 @@ type RuleMatchProtocol =
     | [<JsonUnionCase("tls")>] TLS
     | [<JsonUnionCase("bittorrent")>] BitTorrent
 
+type RuleMatchNetwork =
+    | [<JsonUnionCase("tcp")>] TCP
+    | [<JsonUnionCase("udp")>] UDP
+    | [<JsonUnionCase("tcp,udp")>] TCPAndUDP
+
 type RuleObject =
     { DomainMatcher: DomainMatcher option
       Type: RuleObjectType
@@ -27,7 +32,7 @@ type RuleObject =
       IP: string list option
       Port: string option
       SourcePort: string option
-      Network: string option
+      Network: RuleMatchNetwork option
       Source: string list option
       User: string list option
       Protocol: RuleMatchProtocol option
@@ -40,17 +45,100 @@ type BalancerStrategy =
     | [<JsonUnionCase("random")>] Random
     | [<JsonUnionCase("leastPing")>] LeastPing
 
-type BalancerObject = { Tag: string; Selector: string list; Strategy: BalancerStrategy }
+type BalancerObject =
+    { Tag: string
+      Selector: string list
+      Strategy: BalancerStrategy }
+
 type RoutingObject =
     { DomainStrategy: DomainStrategy option
       DomainMatcher: DomainMatcher option
       Rules: RuleObject list option
       Balancers: BalancerObject list option }
+
 type FullDomainListObject =
     { Direct: string list
       Proxy: string list
       Block: string list }
+
 type DomainList =
     | Full of FullDomainListObject
     | GFWList of string list
     | Greatfire of string list
+
+type RuleConstructionStrategy = // when using full domain list, these options will be ignored
+    { BypassMainland: bool
+      BlockAds: bool }
+
+type ProxyTag =
+    | Outbound of string
+    | Balancer of string
+
+let constructDomainEntry domain = $"domain:{domain}"
+
+let generateRoutingWithDomainList (domainList, constructionStrategy, proxyTag) =
+    match domainList with
+    | Full domainList ->
+        let proxyDomain =
+            domainList.Proxy |> List.map constructDomainEntry
+
+        let directDomain =
+            domainList.Direct |> List.map constructDomainEntry
+
+        let blockDomain =
+            domainList.Block |> List.map constructDomainEntry
+
+        let proxyRule =
+            { DomainMatcher = None
+              Type = Field
+              Domains = Some proxyDomain
+              IP = None
+              Port = None
+              SourcePort = None
+              Network = Some TCPAndUDP
+              Source = None
+              User = None
+              Protocol = None
+              Attrs = None
+              OutboundTag =
+                match proxyTag with
+                | Outbound tag -> Some tag
+                | Balancer _ -> None
+              BalancerTag =
+                match proxyTag with
+                | Balancer tag -> Some tag
+                | Outbound _ -> None }
+
+        let directRule =
+            { DomainMatcher = None
+              Type = Field
+              Domains = Some directDomain
+              IP = None
+              Port = None
+              SourcePort = None
+              Network = Some TCPAndUDP
+              Source = None
+              User = None
+              Protocol = None
+              Attrs = None
+              OutboundTag = Some "freedom"
+              BalancerTag = None }
+
+        let blockRule =
+            { DomainMatcher = None
+              Type = Field
+              Domains = Some blockDomain
+              IP = None
+              Port = None
+              SourcePort = None
+              Network = Some TCPAndUDP
+              Source = None
+              User = None
+              Protocol = None
+              Attrs = None
+              OutboundTag = Some "blackhole"
+              BalancerTag = None }
+
+        [ proxyRule; directRule; blockRule ]
+    | GFWList domainList ->
+        []
