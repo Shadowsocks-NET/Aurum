67a4bf03418c39bd542d260a2facd86e22d03586
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-11-12 10:06:20 +0800

    feature(routing): rules and balancer type

--- Aurum.Configuration/Routing.fs
+++ Aurum.Configuration/Routing.fs
@@ -15,10 +15,29 @@ type DomainMatcher =
 
 type RuleObjectType = | [<JsonUnionCase("field")>] Field
 
+type RuleMatchProtocol =
+    | [<JsonUnionCase("http")>] HTTP
+    | [<JsonUnionCase("tls")>] TLS
+    | [<JsonUnionCase("bittorrent")>] BitTorrent
+
 type RuleObject =
     { DomainMatcher: DomainMatcher option
       Type: RuleObjectType
       Domains: string list option
       IP: string list option
       Port: string option
-      SourcePort: string option }
+      SourcePort: string option
+      Network: string option
+      Source: string list option
+      User: string list option
+      Protocol: RuleMatchProtocol option
+      Attrs: string option
+      OutboundTag: string option
+      BalancerTag: string option }
+
+[<JsonUnion(Mode = UnionMode.CaseKeyAsFieldValue, CaseKeyField = "type")>]
+type BalancerStrategy =
+    | [<JsonUnionCase("random")>] Random
+    | [<JsonUnionCase("leastPing")>] LeastPing
+
+type BalancerObject = { Tag: string; Selector: string list; Strategy: BalancerStrategy }
