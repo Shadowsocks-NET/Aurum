2cc5a26f32cc359a73221b9967dbd4f2508cf91c
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-11-29 13:15:46 +0800

    feature(intermediate): Routing and DNS intermediate types

--- Aurum.Configuration/Intermediate.fs
+++ Aurum.Configuration/Intermediate.fs
@@ -2,6 +2,8 @@ module Aurum.Configuration.Intermediate
 
 open Aurum
 open Aurum.Configuration
+open Aurum.Configuration.DNS
+open Aurum.Configuration.Routing
 
 type SerializedServerConfiguration =
     { Name: string
@@ -9,6 +11,10 @@ type SerializedServerConfiguration =
       Port: int
       Configuration: string
       Type: string }
+
+type SerializedRoutingConfiguration = { Name: string; Configuration: string }
+
+type SerializedDNSConfiguration = { Name: string; Configuration: string }
 
 let serializeServerConfiguration (name, server: Outbound.GenericOutboundObject<Outbound.OutboundConfigurationObject>) =
     let host, port = server.Settings.GetVnextServerInfo()
@@ -19,4 +25,16 @@ let serializeServerConfiguration (name, server: Outbound.GenericOutboundObject<O
       Type = serverType
       Host = host
       Port = port
+      Configuration = configuration }
+
+let serializeRoutingConfiguration (name, routing: RuleObject list) =
+    let configuration = serializeJson routing
+
+    { SerializedRoutingConfiguration.Name = name
+      Configuration = configuration }
+
+let serializeDNSConfiguration (name, dns: DNSObject) =
+    let configuration = serializeJson dns
+
+    { SerializedRoutingConfiguration.Name = name
       Configuration = configuration }
