5e2ebdd203ceb95b7e0b2223f6adf3630d376b67
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-12-01 00:39:35 +0800

    wip(storage): entry action mapper

---
+++ Aurum.Storage/Action.fs
@@ -0,0 +1,13 @@
+﻿module Aurum.Storage.Action
+
+open Aurum
+open Aether
+open Aether.Operators
+
+[<RequireQualifiedAccess>]
+type ConfigurationModificationActions =
+    | Name of string identity
+    | Host of string identity
+    | Port of int identity
+    | Configuration of string identity
+    | Type of string identity
