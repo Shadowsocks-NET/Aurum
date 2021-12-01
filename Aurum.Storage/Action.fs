1b011638d75415aa614561e9c1d52458950f935d
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-12-01 10:42:51 +0800

    feature(storage): fold configurations

--- Aurum.Storage/Action.fs
+++ Aurum.Storage/Action.fs
@@ -12,10 +12,13 @@ type ConfigurationModificationActions =
     | Configuration of string identity
     | Type of string identity
 
-let applyConfiguration action configuration =
+let mapConfiguration configuration action =
     match action with
     | Name action -> Optic.map SerializedServerConfiguration.Name_ action configuration
     | Host action -> Optic.map SerializedServerConfiguration.Host_ action configuration
     | Port action -> Optic.map SerializedServerConfiguration.Port_ action configuration
     | Configuration action -> Optic.map SerializedServerConfiguration.Configuration_ action configuration
     | Type action -> Optic.map SerializedServerConfiguration.Type_ action configuration
+
+let foldConfiguration configuration actions =
+    List.fold mapConfiguration configuration actions
