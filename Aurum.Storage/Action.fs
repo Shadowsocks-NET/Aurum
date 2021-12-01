60f7f9dbd0ab96a5b9a990762da7ac92ab80583e
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-12-01 10:05:53 +0800

    wip(storage): single action application

--- Aurum.Storage/Action.fs
+++ Aurum.Storage/Action.fs
@@ -3,11 +3,19 @@
 open Aurum
 open Aether
 open Aether.Operators
+open Aurum.Configuration.Intermediate
 
-[<RequireQualifiedAccess>]
 type ConfigurationModificationActions =
     | Name of string identity
     | Host of string identity
     | Port of int identity
     | Configuration of string identity
     | Type of string identity
+
+let applyConfiguration action configuration =
+    match action with
+    | Name action -> applyOptics SerializedServerConfiguration.Name_ action configuration
+    | Host action -> applyOptics SerializedServerConfiguration.Host_ action configuration
+    | Port action -> applyOptics SerializedServerConfiguration.Port_ action configuration
+    | Configuration action -> applyOptics SerializedServerConfiguration.Configuration_ action configuration
+    | Type action -> applyOptics SerializedServerConfiguration.Type_ action configuration
