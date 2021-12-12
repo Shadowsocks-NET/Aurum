197661583c3d1dc96cf2c747972ba1f0662cf5c4
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-12-12 11:27:41 +0800

    feature(configuration): Record for top-level JSON configuration

---
+++ Aurum.Configuration/TopLevel.fs
@@ -0,0 +1,26 @@
+﻿module Aurum.Configuration.TopLevel
+
+open Aurum.Configuration.DNS
+open Aurum.Configuration.Inbound
+open Aurum.Configuration.Outbound
+open Aurum.Configuration.Routing
+
+[<RequireQualifiedAccess>]
+type LogLevel =
+    | Debug
+    | Info
+    | Warning
+    | Error
+    | None
+
+type LogObject =
+    { Access: string option
+      Error: string option
+      Loglevel: LogLevel }
+
+type TopLevelObject =
+    { Log: LogObject
+      DNS: DNSObject
+      Routing: RoutingObject
+      Inbounds: InboundObject list
+      Outbounds: OutboundObject list }
