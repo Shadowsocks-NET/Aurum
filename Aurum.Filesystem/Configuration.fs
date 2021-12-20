ce1eef6b4c9e44b91f8d9c7f48e538004e7d472c
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-12-20 09:52:13 +0800

    feature(filesystem): flush configurations to filesystem

--- Aurum.Filesystem/Configuration.fs
+++ Aurum.Filesystem/Configuration.fs
@@ -1,3 +1,34 @@
 ﻿module Aurum.Filesystem.Configuration
 
-let flushConfig = ()
+open System.IO
+open Aurum
+open Aurum.Configuration.TopLevel
+
+let flushConfig (logConf, routingConf, dnsConf, inbounds, outbounds, id) =
+    let topLevel =
+        { Log = logConf
+          Routing = routingConf
+          DNS = dnsConf
+          Inbounds = inbounds
+          Outbounds = outbounds }
+
+    let serializedTopLevel = serializeJson topLevel
+
+    let dataDirectory = getDataDirectory "Aurum"
+
+    if not <| Directory.Exists(dataDirectory) then
+        Directory.CreateDirectory(dataDirectory) |> ignore
+
+    if
+        not
+        <| Directory.Exists(Path.Combine(dataDirectory, "generated"))
+    then
+        Directory.CreateDirectory(Path.Combine(dataDirectory, "generated"))
+        |> ignore
+
+    let confPath =
+        Path.Combine(dataDirectory, "generated", $"{id}.json")
+
+    File.WriteAllText(confPath, serializedTopLevel)
+
+    confPath
