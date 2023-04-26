f51ac85b1d40fd5e291b2eea25c0f9ac2574cc14
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-04-26 21:38:17 +0800

    feature(config/base): unified backend identifier

---
+++ Aurum.Configuration/Base.fs
@@ -0,0 +1,8 @@
+﻿module Aurum.Configuration.Base
+
+open Aurum.Configuration.Sing.Base
+open Aurum.Configuration.V2Fly.Base
+
+type Backend =
+    | SingBox of SingBaseConfiguration
+    | V2fly of V2flyBaseConfiguration
