cc1a6de6c10e8dec25aca071d1daea4b535b93be
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-04-26 17:52:14 +0800

    refactor(config): split common transports into new module

---
+++ Aurum.Configuration/Shared/VMess/Transport.fs
@@ -0,0 +1,19 @@
+﻿module Aurum.Configuration.Shared.VMess.Transports
+
+open System.Text.Json.Serialization
+
+[<RequireQualifiedAccess>]
+type Networks =
+    | [<JsonName("ws")>] WS
+    | [<JsonName("grpc")>] GRPC
+    | [<JsonName("tcp")>] TCP  // merged into http for sing-box
+    | [<JsonName("kcp")>] KCP
+    | [<JsonName("domainsocket")>] DomainSocket
+    | [<JsonName("http")>] HTTP
+    | [<JsonName("quic")>] QUIC
+
+[<RequireQualifiedAccess>]
+type Security =
+    | [<JsonName("none")>] None
+    | [<JsonName("tls")>] TLS
+    | [<JsonName("xtls")>] XTLS  // merely a reservation. V2fly and Sing backend does not have XTLS support.
