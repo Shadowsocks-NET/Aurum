4192d12f7f6a4fae56f4e6829ca3023f1f0a64aa
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-04-26 20:16:45 +0800

    refactor(config/sub): extract subscription config

--- Aurum.Configuration/Sing/Base.fs
+++ Aurum.Configuration/Sing/Base.fs
@@ -9,25 +9,3 @@ type ConfigurationBase =
     outbounds: Outbounds list
     route: obj
     experimental: obj }
-
-type OocApiToken =
-  { version: int
-    baseUrl: string
-    secret: string
-    userId: string }
-
-type CachingPolicy =
-  | Disabled
-  | TTL of int
-  | Diff
-
-type Subscriptions =
-  | Base64 of url: string
-  | Clash of url: string // WIP
-  | OocV1 of apiToken: OocApiToken // WIP
-  | OocSing of apiToken: OocApiToken
-
-type BaseConfiguration =
-  { subscription: Subscriptions list
-    caching: CachingPolicy
-    template: ConfigurationBase }
--- Aurum.Configuration/Subscription.fs
+++ Aurum.Configuration/Subscription.fs
@@ -2,4 +2,16 @@
 
 open System.Net.Http
 
+type OocApiToken =
+  { version: int
+    baseUrl: string
+    secret: string
+    userId: string }
+
+type Subscriptions =
+  | Base64 of url: string
+  | Clash of url: string // WIP
+  | OocV1 of apiToken: OocApiToken // WIP
+  | OocSing of apiToken: OocApiToken
+
 let httpClient = new HttpClient()
