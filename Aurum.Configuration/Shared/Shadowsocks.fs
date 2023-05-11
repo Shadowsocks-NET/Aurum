a286d67bc5f58a3c5fc8b90fca925851f9e8d972
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-05-11 11:41:13 +0800

    refactor(config/shared/ss): embed password in encryption type

--- Aurum.Configuration/Shared/Shadowsocks.fs
+++ Aurum.Configuration/Shared/Shadowsocks.fs
@@ -3,27 +3,26 @@ module Aurum.Configuration.Shared.Shadowsocks
 open System.Text.Json.Serialization
 
 // ShadowsocksR and legacy stream cipher are not supported, since they have serious security issues. If users really wish to use them, they could supply custom JSON config provided their backend has proper support.
+// Password / PSKs are embedded in this DU's fields
 [<RequireQualifiedAccess>]
 type ShadowsocksEncryption =
-    | [<JsonName("none")>] None
-    | [<JsonName("plain")>] Plain
-    | [<JsonName("chacha20-poly1305")>] ChaCha20
-    | [<JsonName("chacha20-ietf-poly1305")>] ChaCha20IETF
-    | [<JsonName("aes-128-gcm")>] AES128
-    | [<JsonName("aes-256-gcm")>] AES256
-    | [<JsonName("2022-blake3-aes-128-gcm")>] AES128_2022
-    | [<JsonName("2022-blake3-aes-256-gcm")>] AES256_2022
-    | [<JsonName("2022-blake3-chacha20-poly1305")>] ChaCha20_2022
-    | [<JsonName("2022-blake3-chacha8-poly1305")>] ChaCha8_2022
+  | [<JsonName("none")>] None
+  | [<JsonName("plain")>] Plain
+  | [<JsonName("chacha20-poly1305")>] ChaCha20 of Password: string
+  | [<JsonName("chacha20-ietf-poly1305")>] ChaCha20IETF of Password: string
+  | [<JsonName("aes-128-gcm")>] AES128 of Password: string
+  | [<JsonName("aes-256-gcm")>] AES256 of Password: string
+  | [<JsonName("2022-blake3-aes-128-gcm")>] AES128_2022 of PSKs: string list
+  | [<JsonName("2022-blake3-aes-256-gcm")>] AES256_2022 of PSKs: string list
+  | [<JsonName("2022-blake3-chacha20-poly1305")>] ChaCha20_2022 of PSKs: string list
+  | [<JsonName("2022-blake3-chacha8-poly1305")>] ChaCha8_2022 of PSKs: string list
 
 type ShadowsocksObject =
   { Host: string
     Port: string
-    Encryption: ShadowsocksEncryption
-    Password: string }
+    Encryption: ShadowsocksEncryption }
 
-let createShadowsocksObject (host, port, encryption, password) =
+let createShadowsocksObject (host, port, encryption) =
   { Host = host
     Port = port
-    Encryption = encryption
-    Password = password }
+    Encryption = encryption }
