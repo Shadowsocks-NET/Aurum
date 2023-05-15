a87274877ca9a0e3e807904e196dc4fd2ef27015
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-05-15 23:21:40 +0800

    feature(config): shared routing rules generator

---
+++ Aurum.Configuration/Shared/Routing.fs
@@ -0,0 +1,214 @@
+module Aurum.Configuration.V2fly.Routing
+
+open Aurum
+open System.Text.Json.Serialization
+
+type DomainStrategy =
+  | [<JsonName("AsIs")>] AsIs
+  | [<JsonName("IPIfNonMatch")>] IPIfNonMatch
+  | [<JsonName("IPOnDemand")>] IPOnDemand
+
+type RuleMatchProtocol =
+  | [<JsonName("http")>] HTTP
+  | [<JsonName("tls")>] TLS
+  | [<JsonName("bittorrent")>] BitTorrent
+
+type RuleMatchNetwork =
+  | [<JsonName("tcp")>] TCP
+  | [<JsonName("udp")>] UDP
+  | [<JsonName("tcp,udp")>] TCPAndUDP
+
+type DomainType =
+  | Domain of string
+  | GeoDomain of string
+
+type IpType =
+  | IP of string
+  | GeoIp of string
+
+type RuleObject =
+  { Domain: DomainType list option
+    Ip: IpType list option
+    Port: string option
+    Networks: RuleMatchNetwork option
+    Protocol: RuleMatchProtocol option
+    Tag: string option }
+
+type DomainRuleList = DomainType list
+type IpRuleList = IpType list
+
+type DomainStringListObject =
+  { Direct: DomainRuleList
+    Proxy: DomainRuleList
+    Block: DomainRuleList }
+
+type IpStringListObject =
+  { Direct: IpRuleList
+    Proxy: IpRuleList
+    Block: IpRuleList }
+
+type RulePresets =
+  | BypassMainland // adopted from v2rayA's mainland whitelist mode
+  | GFWList of string list // uses Loyalsoldier/v2ray-rules-dat's gfw.txt
+  | Greatfire of string list // uses Loyalsoldier/v2ray-rules-dat's greatfire.txt
+  | Loyalsoldier // uses Loyalsoldier/v2ray-rules-dat's recommended v2ray setting in README
+  | Empty // no preset rules
+
+// when using full domain list or the recommended setting, these options will be ignored
+type RuleConstructionStrategy = { BlockAds: bool }
+
+type ProxyTag =
+  | Outbound of string
+  | Balancer of string
+
+type RuleType =
+  | Direct of DomainRuleList option * IpRuleList option
+  | Proxy of DomainRuleList option * IpRuleList option
+  | Block of DomainRuleList option * IpRuleList option
+  | Skip
+
+let constructDirect (domainRule, ipRule) : RuleObject =
+  { Domain = domainRule
+    Ip = ipRule
+    Port = None
+    Networks = Some TCPAndUDP
+    Protocol = None
+    Tag = Some "freedom" }
+
+let constructProxy (domainRule, ipRule) proxyTag : RuleObject =
+  { Domain = domainRule
+    Ip = ipRule
+    Port = None
+    Networks = Some TCPAndUDP
+    Protocol = None
+    Tag = proxyTag }
+
+let constructBlock (domainRule, ipRule) : RuleObject =
+  { Domain = domainRule
+    Ip = ipRule
+    Port = None
+    Networks = Some TCPAndUDP
+    Protocol = None
+    Tag = Some "blackhole" }
+
+let constructSingleRule rule proxyTag : RuleObject option =
+  match rule with
+  | Direct(domainRule, ipRule) -> Some(constructDirect (domainRule, ipRule))
+  | Proxy(domainRule, ipRule) -> Some(constructProxy (domainRule, ipRule) proxyTag)
+  | Block(domainRule, ipRule) -> Some(constructBlock (domainRule, ipRule))
+  | Skip -> None
+
+let constructRuleSet ruleSet proxyTag =
+  List.map (fun rule -> constructSingleRule rule proxyTag) ruleSet
+  |> List.filter Option.isSome
+  |> List.map Option.get
+
+let mergeRules rule1 rule2 =
+  match rule1, rule2 with
+  | Direct(xDomain, xIP), Direct(yDomain, yIP) -> Direct(mergeOptionList xDomain yDomain, mergeOptionList xIP yIP)
+  | Proxy(xDomain, xIP), Proxy(yDomain, yIP) -> Proxy(mergeOptionList xDomain yDomain, mergeOptionList xIP yIP)
+  | Block(xDomain, xIP), Block(yDomain, yIP) -> Block(mergeOptionList xDomain yDomain, mergeOptionList xIP yIP)
+  | _ -> raise RuleTypeNotMatchException
+
+let constructPreset constructionStrategy =
+
+  let blockRule =
+    if constructionStrategy.BlockAds then
+      Block(Some [ GeoDomain "category-ads-all" ], None)
+    else
+      Skip
+
+  blockRule
+
+let generateRoutingRules (domainList, constructionStrategy, userDomainRules: DomainStringListObject, userIpRules) =
+  match domainList with
+  | BypassMainland ->
+    let blockPreset = constructPreset constructionStrategy
+
+    let userProxyRule = Proxy(Some userDomainRules.Proxy, Some userIpRules.Proxy)
+
+    let userDirectRule = Direct(Some userDomainRules.Direct, Some userIpRules.Direct)
+
+    let userBlockRule = Block(Some userDomainRules.Block, Some userIpRules.Block)
+
+    let geolocationProxy = Proxy(Some [ GeoDomain "geolocation-!cn" ], None)
+
+    let scholarProxy = Proxy(Some [ GeoDomain "google-scholar" ], None)
+
+    let scholarDirect =
+      Direct(Some [ GeoDomain "category-scholar-!cn"; GeoDomain "category-scholar-cn" ], None)
+
+    let siteCNDirect = Direct(Some [ GeoDomain "cn" ], None)
+
+    let sarProxy = Proxy(None, Some [ GeoIp "hk"; GeoIp "mo" ])
+
+    let ipDirect = Direct(None, Some [ GeoIp "private"; GeoIp "cn" ])
+
+    constructRuleSet
+      [ mergeRules userBlockRule blockPreset
+        userDirectRule
+        userProxyRule
+        geolocationProxy
+        scholarProxy
+        scholarDirect
+        siteCNDirect
+        sarProxy
+        ipDirect
+        Proxy(None, None) ]
+  | GFWList domainList
+  | Greatfire domainList ->
+    let blockPreset = constructPreset constructionStrategy
+
+    let userBlockRule = Block(Some userDomainRules.Block, Some userIpRules.Block)
+
+    let userDirectRule = Block(Some userDomainRules.Direct, Some userIpRules.Direct)
+
+    let userProxyRule = Proxy(Some userDomainRules.Proxy, Some userIpRules.Proxy)
+
+    let proxyRule = Proxy(Some(domainList |> List.map (fun a -> Domain a)), None)
+
+    constructRuleSet
+      [ mergeRules userBlockRule blockPreset
+        userDirectRule
+        mergeRules userProxyRule proxyRule
+        Proxy(None, None) ]
+  | Loyalsoldier ->
+    let userBlockRule = Block(Some userDomainRules.Block, Some userIpRules.Block)
+
+    let userDirectRule = Block(Some userDomainRules.Direct, Some userIpRules.Direct)
+
+    let userProxyRule = Proxy(Some userDomainRules.Proxy, Some userIpRules.Proxy)
+
+    let blockRule = Block(Some([ GeoDomain "category-ads-all" ]), None)
+
+    let directRule1 =
+      Direct(
+        Some(
+          [ GeoDomain "private"
+            GeoDomain "apple-cn"
+            GeoDomain "google-cn"
+            GeoDomain "tld-cn"
+            GeoDomain "category-games@cn" ]
+        ),
+        None
+      )
+
+    let proxyRule = Proxy(Some([ GeoDomain"geolocation-!cn" ]), None)
+
+    let directRule2 = Direct(Some([ GeoDomain"cn" ]), None)
+
+    constructRuleSet
+      [ mergeRules userBlockRule blockRule
+        userDirectRule
+        userProxyRule
+        directRule1
+        proxyRule
+        directRule2
+        Proxy(None, None) ]
+  | Empty ->
+    let blockPreset = constructPreset constructionStrategy
+
+    constructRuleSet
+      [ mergeRules blockPreset (Block(Some userDomainRules.Block, Some userIpRules.Block))
+        Direct(Some userDomainRules.Direct, Some userIpRules.Direct)
+        Proxy(Some userDomainRules.Proxy, Some userIpRules.Proxy) ]
