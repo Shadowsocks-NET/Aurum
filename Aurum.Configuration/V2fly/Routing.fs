fe3159909e87aaf83aa4bc3656e7661d12a15821
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-05-17 13:20:20 +0800

    feature(config/v2fly/routing): v2fly v5 routing format This routing config format is not the exact format of v2ray-core v5, but a saner version that I have proposed to their developers for easier client implementation

--- Aurum.Configuration/V2fly/Routing.fs
+++ Aurum.Configuration/V2fly/Routing.fs
@@ -4,90 +4,59 @@ open Aurum
 open System.Text.Json.Serialization
 
 type DomainStrategy =
-    | [<JsonName("AsIs")>] AsIs
-    | [<JsonName("IPIfNonMatch")>] IPIfNonMatch
-    | [<JsonName("IPOnDemand")>] IPOnDemand
+  | [<JsonName("AsIs")>] AsIs
+  | [<JsonName("IPIfNonMatch")>] IPIfNonMatch
+  | [<JsonName("IPOnDemand")>] IPOnDemand
 
 type DomainMatcher =
-    | [<JsonName("mph")>] MinimalPerfectHash
-    | [<JsonName("linear")>] Original
+  | [<JsonName("mph")>] MinimalPerfectHash
+  | [<JsonName("linear")>] Original
 
 type RuleMatchProtocol =
-    | [<JsonName("http")>] HTTP
-    | [<JsonName("tls")>] TLS
-    | [<JsonName("bittorrent")>] BitTorrent
+  | [<JsonName("http")>] HTTP
+  | [<JsonName("tls")>] TLS
 
 type RuleMatchNetwork =
-    | [<JsonName("tcp")>] TCP
-    | [<JsonName("udp")>] UDP
-    | [<JsonName("tcp,udp")>] TCPAndUDP
+  | [<JsonName("tcp")>] TCP
+  | [<JsonName("udp")>] UDP
+  | [<JsonName("tcp,udp")>] TCPAndUDP
+
+[<JsonFSharpConverter(BaseUnionEncoding = JsonUnionEncoding.AdjacentTag)>]
+type DomainObject =
+  | Plain of string
+  | Regex of string
+  | RootDomain of string
+  | Full of string
+
+type GeoDomainObject =
+  { InverseMatch: bool option
+    Code: string option
+    FilePath: string option }
+
+type CidrObject = { IpAddr: string; Prefix: int }
+
+type GeoIpObject =
+  { InverseMatch: bool option
+    Code: string option
+    FilePath: string option }
 
 type RuleObject =
-    { DomainMatcher: DomainMatcher option
-      Domain: string list option
-      GeoDomain: string list option
-      Ip: string list option
-      Port: string option
-      SourcePort: string option
-      Network: RuleMatchNetwork option
-      Source: string list option
-      User: string list option
-      Protocol: RuleMatchProtocol option
-      Attrs: string option
-      OutboundTag: string option
-      BalancerTag: string option }
-
-[<JsonFSharpConverter(BaseUnionEncoding = JsonUnionEncoding.AdjacentTag, UnionTagName = "type")>]
-type BalancerStrategy =
-    | [<JsonName("random")>] Random
-    | [<JsonName("leastPing")>] LeastPing
-
-type BalancerObject =
-    { Tag: string
-      Selector: string list
-      Strategy: BalancerStrategy }
+  { Tag: string
+    Domain: DomainObject list option
+    GeoDomain: GeoDomainObject list option
+    Ip: CidrObject list option
+    GeoIp: GeoIpObject list option
+    Networks: RuleMatchNetwork
+    PortList: string option
+    InboundTag: string option }
 
 type RoutingObject =
-    { DomainStrategy: DomainStrategy option
-      DomainMatcher: DomainMatcher option
-      Rules: RuleObject list option
-      Balancers: BalancerObject list option }
+  { DomainStrategy: DomainStrategy option
+    DomainMatcher: DomainMatcher option
+    Rules: RuleObject list option }
 
-type DomainRuleList = string list
-type IpRuleList = string list
+let createRoutingObject rules domainStrategy =
+  { RoutingObject.Rules = rules
+    DomainMatcher = Some MinimalPerfectHash
+    DomainStrategy = domainStrategy }
 
-type DomainStringListObject =
-    { Direct: DomainRuleList
-      Proxy: DomainRuleList
-      Block: DomainRuleList }
-
-type IpStringListObject =
-    { Direct: IpRuleList
-      Proxy: IpRuleList
-      Block: IpRuleList }
-
-type RulePresets =
-    | BypassMainland // adopted from v2rayA's mainland whitelist mode
-    | GFWList of string list // uses Loyalsoldier/v2ray-rules-dat's gfw.txt
-    | Greatfire of string list // uses Loyalsoldier/v2ray-rules-dat's greatfire.txt
-    | Loyalsoldier // uses Loyalsoldier/v2ray-rules-dat's recommended v2ray setting in README
-    | Empty // no preset rules
-
-// when using full domain list or the recommended setting, these options will be ignored
-type RuleConstructionStrategy = { BlockAds: bool }
-
-type ProxyTag =
-    | Outbound of string
-    | Balancer of string
-
-type RuleType =
-    | Direct of DomainRuleList option * IpRuleList option
-    | Proxy of DomainRuleList option * IpRuleList option
-    | Block of DomainRuleList option * IpRuleList option
-    | Skip
-
-let createRoutingObject rules balancers domainStrategy =
-    { RoutingObject.Balancers = balancers
-      Rules = rules
-      DomainMatcher = Some MinimalPerfectHash
-      DomainStrategy = domainStrategy }
